560accc1187cc417fb6050f6a8dcef4d9e883e03
soraluz <rijasoag23@hotmail.com>
2023-01-04 23:04:44 -0500

    ejercicio_bucles

--- sesion-01-02-mi-primer-proyecto-de-node/ejercicio2.js
+++ sesion-01-02-mi-primer-proyecto-de-node/ejercicio2.js
@@ -1,2 +1,4 @@
 const persona=["Ricardo Soraluz" ,42 ,true ,"19/01/1980",{ titulo:"La biblia", author:"Dios", fecha:"01/01/0001", url:"www.labible,com"}]
 console.log(persona)
+var s=3;
+console.log(s);
