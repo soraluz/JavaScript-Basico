b336f674b4c162b0865e1ef7f9295f24667e423f
soraluz <rijasoag23@hotmail.com>
2023-01-05 15:09:01 -0500

    ejercicio numbers

---
+++ sesion-05-numbers/ejercicio_numbers.js
@@ -0,0 +1,12 @@
+let altura=165;
+let alturaFloat=1.65;
+let peso=70.2;
+let alturaR=Math.ceil(alturaFloat,2);
+let pesoR=Math.floor(peso,2);
+console.log(alturaFloat);
+console.log(peso);
+console.log(alturaR);
+console.log(pesoR);
+
+
+
