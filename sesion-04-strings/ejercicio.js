43c4b7cca02a1780ec151ecd3affb197f61ae681
soraluz <rijasoag23@hotmail.com>
2023-01-05 14:17:10 -0500

    ejercicio strings

---
+++ sesion-04-strings/ejercicio.js
@@ -0,0 +1,21 @@
+let nombre="Ricardo";
+let apellido="Soraluz";
+
+let estudiante= nombre + " " + apellido;
+let estudianteMayus=estudiante.toUpperCase(estudiante);
+let estudianteMinus= estudiante.toLowerCase(estudiante);
+let lonEst=estudiante.length;
+let firstNombre=nombre[0];
+let lastApellido=apellido[apellido.length-1];
+let estudianteSinEspacios=estudiante.trim();
+let isNombre=estudiante.includes(nombre);
+
+console.log(estudiante)
+console.log(estudianteMayus)
+console.log(estudianteMinus)
+console.log(lonEst)
+console.log(firstNombre)
+console.log(lastApellido)
+console.log(estudianteSinEspacios)
+console.log(isNombre)
+
