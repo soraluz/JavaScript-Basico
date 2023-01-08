7528d40d9c7fc6f135221653de7d8044e5d53ac9
soraluz <rijasoag23@hotmail.com>
2023-01-07 23:08:05 -0500

    ejercicio eventos

---
+++ sesion-14-js-html/ejercicio/ejercicio.js
@@ -0,0 +1,8 @@
+const boton=document.querySelector("#btn")
+boton.addEventListener("click",()=>{
+    alert("click en el boton");
+})
+
+$(".btn").click(()=>{
+    console.log("Hola, estoy utilizando jQuery")
+})
